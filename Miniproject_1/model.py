970e6163bd2b2d1708cc03ba0061462adce174d2
Stefan <stefanigescu@gmail.com>
2022-04-28 14:01:29 +0200

    add model skeleton

---
+++ Miniproject_1/model.py
@@ -0,0 +1,23 @@
+### For mini-project 1
+
+class Model():
+    def __init__(self) -> None:
+        ## instantiate model + optimizer + loss function + any other stuff you need
+        pass
+
+    def load_pretrained_model(self) -> None:
+        ## This loads the parameters saved in bestmodel .pth into the model
+        pass
+
+    def train(self, train_input, train_target, num_epochs) -> None:
+        #: train_input: tensor of size (N,C,H,W) containing a noisy version of the images
+
+        #: train_target: tensor of size (N, C, H, W) containing another noisy version of the same images, which only differs the input by their noise.
+
+        pass
+
+    def predict(self, test_input) -> torch.Tensor:
+        #: test˙input : tensor of size (N1 , C, H, W) that has to be denoised by the trained or the loaded network .
+        #: returns a tensor of the size (N1 , C, H, W)
+
+        pass
