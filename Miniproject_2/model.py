847ad12a1caa356cf202cee56be0782f724aca72
giommok <giovanni.monea@epfl.ch>
2022-05-23 15:26:16 +0200

    fixino

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -30,8 +30,8 @@ class Model:
         pass
 
     def train(self, train_input, train_target, num_epochs) -> None:
-        train_input = train_input / 256
-        train_target = train_target / 256
+        # train input in range [0, 255], output of network in range [0, 1], train target in range [0, 255]
+        train_target = train_target / 255.0 # fix range of train target
         # train_input: tensor of size (N,C,H,W) containing a noisy version of the images
         # train_target: tensor of size (N, C, H, W) containing another noisy version of the same images, which only differs the input by their noise.
         for e in range(num_epochs):
@@ -49,10 +49,10 @@ class Model:
         inp, target = train_input, train_target
         output = self.model.forward(inp)
         loss = self.loss.forward(preds=output, labels=target)
-        print("EPOCH {} --- FINAL LOSS {}".format(e, loss))
+        print("FINAL LOSS {}".format(loss))
 
     def predict(self, test_input) -> torch.Tensor:
         #: test˙input : tensor of size (N1 , C, H, W) that has to be denoised by the trained or the loaded network .
         #: returns a tensor of the size (N1 , C, H, W)
-
-        return (self.model.forward(test_input / 256) * 256).round()
+        # test input in range [0, 255], output in range [0, 255]
+        return self.model.forward(test_input) * 255.0
